f8ba3e0d16fbcc4af23e0496799cc164a897081e
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2022-07-14 18:34:44 +1200

    Try implement structures. DOESNT WORK

---
+++ src/main/java/chiefarug/mods/geysers_not_included/GeyserFeature.java
@@ -0,0 +1,70 @@
+package chiefarug.mods.geysers_not_included;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.NoiseColumn;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.feature.JigsawFeature;
+import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
+import net.minecraft.world.level.levelgen.structure.PoolElementStructurePiece;
+import net.minecraft.world.level.levelgen.structure.PostPlacementProcessor;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGenerator;
+import net.minecraft.world.level.levelgen.structure.pieces.PieceGeneratorSupplier;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class GeyserFeature extends StructureFeature<JigsawConfiguration> {
+
+
+	public GeyserFeature() {
+		super(JigsawConfiguration.CODEC, GeyserFeature::createPiecesGenerator);
+	}
+
+	public static Optional<PieceGenerator<JigsawConfiguration>> createPiecesGenerator(PieceGeneratorSupplier.Context<JigsawConfiguration> context) {
+		// Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
+		ChunkPos chunkPos = context.chunkPos();
+		NoiseColumn column = context.chunkGenerator().getBaseColumn(chunkPos.getMiddleBlockX(), chunkPos.getMiddleBlockZ(), context.heightAccessor());
+		int minY = context.heightAccessor().getMinBuildHeight();
+		int maxY = 100;
+
+		int currentY = minY;
+		boolean caveFound = false;
+		while (!caveFound) {
+			if (currentY > maxY) break;
+			caveFound = column.getBlock(currentY).isAir();
+			currentY++;
+		}
+
+		BlockPos genPos = chunkPos.getBlockAt(chunkPos.getMiddleBlockX(), currentY, chunkPos.getMiddleBlockZ());
+		Optional<PieceGenerator<JigsawConfiguration>> structurePiecesGenerator = JigsawPlacement.addPieces(
+				context,
+				PoolElementStructurePiece::new,
+				genPos.above(100),
+				false,
+				false);
+
+		if (currentY > 60) {
+			structurePiecesGenerator = Optional.empty();
+			GeysersNotIncluded.LOGGER.debug("Tried to generate a geyser on ground level at {}", genPos);
+		}
+
+		if(structurePiecesGenerator.isPresent()) {
+			// I use this to debug and quickly find out if the structure is spawning or not and where it is.
+			// This is returning the coordinates of the center starting piece.
+			GeysersNotIncluded.LOGGER.debug("Geyser located at {}", genPos);
+		}
+
+		// Return the pieces generator that is now set up so that the game runs it when it needs to create the layout of structure pieces.
+		return structurePiecesGenerator;
+	}
+
+	@Override
+	public GenerationStep.Decoration step() {
+		return GenerationStep.Decoration.SURFACE_STRUCTURES;
+	}
+}
--- src/main/java/chiefarug/mods/geysers_not_included/Registry.java
+++ src/main/java/chiefarug/mods/geysers_not_included/Registry.java
@@ -2,6 +2,7 @@ package chiefarug.mods.geysers_not_included;
 
 import chiefarug.mods.geysers_not_included.blocks.GeyserBlock;
 import chiefarug.mods.geysers_not_included.blocks.GeyserBlockEntity;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -10,6 +11,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.levelgen.feature.StructureFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.JigsawConfiguration;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -17,6 +20,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
 
 import static chiefarug.mods.geysers_not_included.GeysersNotIncluded.MODID;
 
@@ -36,7 +41,9 @@ public class Registry {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MODID);
+	public static final DeferredRegister<StructureFeature<?>> STRUCTURE_FEATURES = DeferredRegister.create(new ResourceLocation("minecraft:worldgen/structure_feature"), MODID);
 
+	public static final RegistryObject<StructureFeature<JigsawConfiguration>> GEYSER = STRUCTURE_FEATURES.register("geyser", GeyserFeature::new);
 
 	public static final RegistryObject<GeyserBlock> GEYSER_BLOCK = BLOCKS.register("geyser", () -> new GeyserBlock(BLOCK_PROPERTIES));
 	public static final RegistryObject<BlockEntityType<GeyserBlockEntity>> GEYSER_BLOCK_ENTITY = BLOCK_ENTITIES.register("geyser", () -> BlockEntityType.Builder.of(GeyserBlockEntity::new, GEYSER_BLOCK.get()).build(null));
@@ -47,5 +54,6 @@ public class Registry {
 		BLOCKS.register(bus);
 		ITEMS.register(bus);
 		BLOCK_ENTITIES.register(bus);
+		STRUCTURE_FEATURES.register(bus);
 	}
 }
